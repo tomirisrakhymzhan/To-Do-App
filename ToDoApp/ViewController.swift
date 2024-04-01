cb7d7fe425bcfc92a25cccc759403d3254dc6763
tomiris.rakhymzhan <tomiris.rakhymzhan@gmail.com>
2024-04-01 18:02:54 +0500

    Initial Commit

---
+++ ToDoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoApp
+//
+//  Created by Томирис Рахымжан on 01/04/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
